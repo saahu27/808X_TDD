3c39f370e5d097afe36b2526f57169ce0149589d
Sahruday <89718768+saahu27@users.noreply.github.com>
2022-10-02 18:33:12 -0400

    added comments and updated UML and activity

--- include/TDD.hpp
+++ include/TDD.hpp
@@ -1,46 +1,92 @@
 /**
- * @file grades.hpp
- * @author Divyansh Agrawal (dagrawa1@umd.edu)
+ * @file TDD.hpp
+ * @author Divyansh Agrawal (dagrawa1@umd.edu) Sahruday Patti (sahruday@umd.edu)
  * @brief header file
  * @version 0.1
- * @date 2022-09-20
+ * @date 2022-09-29
  * 
  * @copyright Copyright (c) 2022
  * 
  */
 #pragma once
-// #include <array>
+
 #include <iostream>
-// #include <vector>
-// #include <numeric>
-// #include <string>
+
+/** @namespace controller TDD.hpp
+ *  "include/../include/TDD.hpp"
+ *  @brief Utility functions for PID
+ */
 
 namespace controller {
-   
+   /**
+    * @brief class PID contains utility functions for developing a PID controller
+    */
 class PID{
  public:
-   // PID();
+
+    /**
+     * @brief Getter for private variable errorPrevious_
+     * @return errorPrevious_
+     */
 
     double GetterErrorPrevious();
 
+    /**
+     * @brief Setter for private variable errorPrevious_
+     * @param error the present error at the end of loop is set as previous error
+     */
+
     void SetterErrorPrevious(double Error);
 
+    /**
+     * @brief method to compute to reach the setpoint
+     * @param targetVel the velocity to which we need to converge
+     * @param currentVel the current velocity from which it should reach the setpoint value
+     * @return targetVel within the tolerance
+     */
     double ComputeVel(double targetVel, double currentVel);
 
+    /**
+     * @brief method to compute to reach the setpoint
+     * @return returns the time step set
+     */
     double returndt();
-   
-  //  ~PID();
    
 
  private:
+    /**
+     * @brief integral constant chosen
+     * @param ki_ private variable chosen
+     */
+    const double ki_ = 0.1; 
 
-    const double ki_ = 0.1;  //
+    /**
+     * @brief proportional constant chosen
+     * @param kp_ private variable chosen and set
+     */
     const double kp_ = 0.01;  //
-    const double kd_ = 0.001;  //
-    const double dt_ = 1;  //
-    double errorPrevious_;  //
+
+    /**
+     * @brief derivative constant chosen
+     * @param kd_ private variable chosen and set
+     */
+    const double kd_ = 0.001;  
+
+    /**
+     * @brief timestep chosen
+     * @param dt_ private variable chosen
+     */
+    const double dt_ = 1;  
+    /**
+     * @brief variable to update the previous error
+     * @param errorPrevious_
+     */
+    double errorPrevious_;  
+    /**
+     * @brief tolerance level chosen
+     * @param tolerance_ private variable chosen and set
+     */
     const double tolerance_ = 0.1;  //
     
-
-};
+  };
 }  // namespace controller
