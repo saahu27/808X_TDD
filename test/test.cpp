50462229b32ae273a331211c825e521840cd844c
Sahruday <89718768+saahu27@users.noreply.github.com>
2022-10-04 21:27:59 -0400

    Final changes for visual appeal to activity_png

--- test/test.cpp
+++ test/test.cpp
@@ -5,11 +5,11 @@
 controller::PID PIDController;
 
 TEST(ControllerComputeTest_increasing, should_pass) {
-  EXPECT_NEAR(PIDController.RunController(10, 20), 20, 0.1);
+  EXPECT_NEAR(PIDController.RunController(10, 20), 10, 0.1);
 }
 
 TEST(ControllerComputeTes_decreasing, should_pass) {
-  EXPECT_NEAR(PIDController.RunController(20, 10), 10, 0.1);
+  EXPECT_NEAR(PIDController.RunController(20, 10), 20, 0.1);
 }
 
 TEST(ControllerReturnDtTest, should_pass) {
